f1e39ae787b24b555b2b1f75983e71b71e7fb747
Neil Ning <ningcoder@foxmail.com>
2021-10-20 00:09:02 +0800

    Project init

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+program.command('create <app-name>')
+  .description('Create a new project')
+  .option('-f --force', 'Overwrite the existing dir')
+  .action((name, options) => {
+    require('../libs/create')(name, options);
+  });
+
+program.command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  });
+
+program.on('--help', () => {
+  console.log('\r\n' + figlet.textSync('React Skeleton'));
+  console.log(`\r\nRun ${chalk.cyan(`rs <command> --help`)} show details\r\n`);
+});
+
+program.version(`v${require('../package.json').version}`)
+  .usage('<command> [option]');
+
+program.parse(process.argv);
+
